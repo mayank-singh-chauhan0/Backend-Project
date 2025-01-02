65fb81a5ae12d96391940ff1ddb3041b466b8e78
mayank-singh-chauhan0 <ms0798041@gmail.com>
2025-01-02 21:56:37 +0530

    Add comment, like, playlist, and tweet models with CRUD operations

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,175 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js";
+import { Comment } from "../models/comment.model.js";
+import mongoose from "mongoose";
+
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  if (!videoId) {
+    throw new ApiError(400, "Please provide a valid video Id");
+  }
+  const getComment = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "user",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "$owner",
+        },
+        isLiked: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$likes.likedBy"],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        likesCount: 1,
+        owner: {
+          username: 1,
+          fullName: 1,
+          "avatar.url": 1,
+        },
+        isLiked: 1,
+      },
+    },
+  ]);
+  if (!getComments) {
+    throw new ApiError(500, "Error while loading getComments section");
+  }
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+
+  const comments = await Comment.aggregatePaginate(getComments, options);
+
+  if (!comments) {
+    throw new ApiError(500, "Error while loading comments section");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments fetched successfully!"));
+});
+
+const addComment = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const comment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user?._id,
+  });
+
+  if (!comment) {
+    throw new ApiError(500, "Failed to add comment please try again");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "Comment added successfully"));
+});
+
+const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+  if (!content) {
+    throw new ApiError(400, "Content is required");
+  }
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not found");
+  }
+
+  if (comment?.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not allowed to update this comment");
+  }
+  const updatedComment = await Comment.findByIdAndUpdate(
+    comment?._id,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatedComment) {
+    throw new ApiError(500, "Failed to edit comment please try again");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment edited successfully", updateComment));
+});
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment not loaded");
+  }
+  if (comment?.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "You are not allowed to delete this comment");
+  }
+  const deleteComm = await Comment.findByIdAndDelete(comment?._id);
+  await Like.deleteMany({
+    comment: commentId,
+    likedBy: req.user,
+  });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment edited successfully"));
+});
+
+export { addComment, updateComment, deleteComment };
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -2,6 +2,7 @@ import mongoose,{Schema} from 'mongoose';
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
+   
     title: {
         type: String,
         required: true
