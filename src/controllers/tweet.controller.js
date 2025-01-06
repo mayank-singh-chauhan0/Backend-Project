b1b492549d40b5abfb25a3ece403f1ae8d427ffd
mayank-singh-chauhan0 <ms0798041@gmail.com>
2025-01-06 14:14:08 +0530

    Enhance tweet controller with user validation, aggregation for tweets, and improved response structure

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -26,7 +26,85 @@ return res
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-     
+    const { userId } = req.params;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid userId");
+    }
+
+    const tweets = await Tweet.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as: "likeDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            likedBy: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                likesCount: {
+                    $size: "$likeDetails",
+                },
+                ownerDetails: {
+                    $first: "$ownerDetails",
+                },
+                isLiked: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$likeDetails.likedBy"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            },
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+        {
+            $project: {
+                content: 1,
+                ownerDetails: 1,
+                likesCount: 1,
+                createdAt: 1,
+                isLiked: 1
+            },
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
@@ -35,15 +113,19 @@ const updateTweet = asyncHandler(async (req, res) => {
   if (!isValidObjectId(tweetId)) {
     throw new ApiError(400, "Invalid tweetId");
 }
+// console.log(tweetId  , "cew"); 
   if(content.toString().trim() === "") throw new ApiError (400 , "Contnet is Required");
-  if(!isValidObjectId(user?._id)) throw new ApiError (400 , "User is not valid");
-   const user = await Tweet.findById(tweetId);
+//   console.log(tweetId  , "fr4e"); 
+  const user = await Tweet.findById(tweetId);
+   
+   if(!isValidObjectId(user?._id)) throw new ApiError (400 , "User is not valid");
+
    if (!user) {
     throw new ApiError(404, "Tweet not found");
 }
    if(req.user?._id.toString()!==user.owner.toString()) throw new ApiError (400 , " not valid user");
-
-   const updatedTweet = Tweet.findByIdAndUpdate(
+   
+   const updatedTweet = await Tweet.findByIdAndUpdate(
     tweetId ,
     {
         $set : {
@@ -52,10 +134,11 @@ const updateTweet = asyncHandler(async (req, res) => {
     },
     { new: true }
    )
+//    console.log( user.owner ,"wede" ,req.user , "failed" );
    if (!updatedTweet) {
     throw new ApiError(500, "Failed to edit tweet please try again");
 }
-
+// console.log( updateTweet.content , "dwq" );
 return res
     .status(200)
     .json(new ApiResponse(200, "Tweet updated successfully" , updatedTweet));
@@ -73,14 +156,14 @@ const deleteTweet = asyncHandler(async (req, res) => {
   }
      if(req.user?._id.toString()!==user.owner.toString()) throw new ApiError (400 , " not valid user");
   
-     const deletedTweet = Tweet.findByIdAndDelete(
+      await Tweet.findByIdAndDelete(
       tweetId 
      )
     
   
   return res
       .status(200)
-      .json(new ApiResponse(200, "Tweet updated successfully" , {tweetId}));
+      .json(new ApiResponse(200, "Tweet deleted successfully" , {tweetId}));
 })
 
 export {
